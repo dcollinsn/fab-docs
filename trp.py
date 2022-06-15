ab9104b651b0f0110f619bbb6c7f1448843b97eb
Dan Collins <dcollinsn@gmail.com>
2022-06-14 23:10:52 -0400

    Various formatting improvements

--- trp.py
+++ trp.py
@@ -1,6 +1,7 @@
 import requests
 from markdownify import markdownify
 from bs4 import BeautifulSoup
+import re
 
 TRP_URL = "https://fabtcg.com/resources/rules-and-policy-center/tournament-rules-and-policy/"
 r = requests.get(TRP_URL)
@@ -8,7 +9,28 @@ r.raise_for_status()
 
 soup = BeautifulSoup(r.content, 'html.parser')
 content = soup.find('div', class_='page-content')
-print(content)
 md = markdownify(str(content))
+
+# Create section anchor links
+md = re.sub(r'^(#+) (\d[.\d\w]*?)\.? (.+?)$',
+            r'\1 \2 \3 <a href="#\2" id="\2">#</a>',
+            md,
+            flags=re.M,
+)
+
+# Fix extra whitespace in tables
+md = re.sub(r'\|\s{2,}(.+?)\s+(?=\|)',
+            r'| \1 ',
+            md,
+            flags=0,
+)
+
+# No newlines in links
+md = re.sub(r'\n(?=[^\[]+\])', '', md)
+
+# Collapse whitespace
+md = re.sub(r'[ \t]+', ' ', md)
+md = re.sub(r'\n\n+', '\n\n', md)
+
 with open('trp.md', 'w') as f:
     f.write(md)
